64286578bba3d2e7699f855a9b136338f6c57756
xxxxxx <xxxxxx@abc.com>
2017-04-09 20:36:42 +0800

    hibernate naming strategy

---
+++ src/main/java/com/soak/framework/orm/hibernate/UnderscoreNamingStrategy.java
@@ -0,0 +1,29 @@
+package com.soak.framework.orm.hibernate;
+
+import org.hibernate.cfg.ImprovedNamingStrategy;
+import org.hibernate.cfg.NamingStrategy;
+
+public class UnderscoreNamingStrategy extends ImprovedNamingStrategy implements NamingStrategy {
+  private static final long serialVersionUID = 1L;
+
+  @Override
+  public String classToTableName(String className) {
+    return "T_" + tableName(className);
+  }
+
+  @Override
+  public String propertyToColumnName(String propertyName) {
+    return addUnderscores(propertyName).toUpperCase();
+  }
+
+  @Override
+  public String columnName(String columnName) {
+    return addUnderscores(columnName).toUpperCase();
+  }
+
+  @Override
+  public String tableName(String tableName) {
+    return addUnderscores(tableName).toUpperCase();
+  }
+
+}
